64b85d8e76f08f323cdc19f9ad00d518af60a663
kawaset <kawaset@kawaset-ws2.hhmi.org>
2023-06-06 14:23:21 -0400

    update the launcher

--- fluorender/Linux/launcher.py
+++ fluorender/Linux/launcher.py
@@ -8,10 +8,17 @@ else:
 print( 'bundle dir is', bundle_dir )
 print( 'sys.executable is', os.path.dirname(sys.executable) )
 
+print(f"Arguments count: {len(sys.argv)}")
+for i, arg in enumerate(sys.argv):
+    print(f"Argument {i:>6}: {arg}")
+
 dir = os.path.dirname(sys.executable)
 
 command = "export GDK_PIXBUF_MODULE_FILE=" + dir + "/data/loaders.cache" + " && "
 command += "export XDG_DATA_DIRS=\"" + dir + "/data:" + dir + "/data/schemas:" + "/usr/shre/gnome:/usr/local/share:/usr/share\"" + " && "
-command += dir + "/data/VVDViewer"
+command += dir + "/data/VVDViewer "
+for i, arg in enumerate(sys.argv):
+    if i > 0:
+        command += arg + " "
 
 os.system(command)
