baba7d70757de4e759a11301b7218ae4ffd74c5e
Install <Install@KAWASET-WW2.hhmi.org>
2024-10-16 12:15:35 -0400

    update version number

--- fluorender/FluoRender/Main.cpp
+++ fluorender/FluoRender/Main.cpp
@@ -101,7 +101,7 @@ bool VRenderApp::OnInit()
       std::string(VERSION_MAJOR_TAG) +  std::string(".") +
       std::string(VERSION_MINOR_TAG);
 */   
-   std::string title =  std::string(FLUORENDER_TITLE) + "1.6.9";
+   std::string title =  std::string(FLUORENDER_TITLE) + "1.7.0";
 
    m_frame = new VRenderFrame(
 	     this,
